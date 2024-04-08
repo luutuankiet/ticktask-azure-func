7ec79a6854d23b090b4a56b24417074b8d6c00fc
luutuankiet <luutuankiet.ftu2@gmail.com>
2024-04-08 04:41:29 -0500

    done boiler code

--- function_app.py
+++ function_app.py
@@ -1,5 +1,6 @@
 import azure.functions as func
 import logging
+import requests
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
@@ -7,19 +8,31 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 def init(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    html_form = """
+    <html>
+    <body>
+    <form action="/api/getinput" method="post">
+        <label for="input_text">Enter Text:</label><br>
+        <input type="text" id="input_text" name="input_text"><br>
+        <input type="submit" value="Submit">
+    </form>
+    </body>
+    </html>
+    """
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    return func.HttpResponse(html_form, mimetype="text/html")
+
+@app.route(route="getinput", methods=['POST'])
+def getinput(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    input_text = req.form.get('input_text')
+
+    # Make an HTTP POST request to the webhook URL
+    webhook_url = "https://hook.us1.make.com/mt0sveegbtqbsdhgobsqtph9nycwx7t6"
+    response = requests.post(webhook_url, f'{input_text}')
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        return func.HttpResponse("Input sent to webhook successfully", status_code=200)
     else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        return func.HttpResponse("Failed to send input to webhook", status_code=500)
